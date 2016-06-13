4d0102e042e571bec010c795a55e4edbd611ec87
Rajush Shakya <rajushshakya@gmail.com>
2016-06-12 22:28:17 -0700

    adding db connection comment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+// Connecting to mongodb using mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cats');
 
