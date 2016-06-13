accd7b478369f8fd7ed34108c861a90e7499e7dc
Rajush Shakya <rajushshakya@gmail.com>
2016-06-12 22:01:07 -0700

    connecting to mongodb

--- cat_routes.js
+++ cat_routes.js
@@ -1,49 +1,71 @@
 var _ = require('lodash');
-//var Cat = require('./cat_model.js');
+var Cat = require('./cat_model.js');
 
 module.exports = function(app){
     _cats = [];
 
     // Create
     app.post('/cat', function(req, res){
-        _cats.push(req.body);
-        res.json({info: 'cat created successfully'});
+        var newCat = new Cat(req.body);
+        newCat.save(function(err){
+            if (err) {
+                res.json({info: 'error during cat create', error: err});
+            }
+            res.json({info: 'cat created successfully'});
+        });
     });
 
     // Read
     app.get('/cat', function(req, res){
-        res.send(_cats);
+        Cat.find(function(err, cats){
+            if (err) {
+                res.json({info: 'error during find cats', error: err});
+            }
+            res.json({info: 'cats found successfully', data: cats});
+        });
     });
 
     app.get('/cat/:id', function(req, res){
-        res.send(
-            _.find(
-                _cats,
-                {
-                    name: req.params.id
-                }
-            )
-        );
+        Cat.find(function(err, cat){
+            if (err) {
+                res.json({info: 'error during find cat', error: err});
+            }
+            if (cat) {
+                res.json({info: 'cat found successfully', data: cat});
+            }else{
+                res.json({info: 'cat not found'});
+            }
+        });
     });
 
     // Update
     app.put('/cat/:id', function(req, res){
-        var index = _.findIndex(
-            _cats,
-            {
-                name: req.params.id
+        Cat.findById(req.params.id, function(err, cat){
+            if (err) {
+                res.json({info: 'error during find cat', error: err});
             }
-        );
-        _.merge(_cats[index], req.body);
-        res.json({info: 'cat updated successfully'});
+            if (cat) {
+                _.merge(cat, req.body);
+                cat.save(function(err){
+                    if (err) {
+                        res.json({info: 'error during cat update', error: err});
+                    }
+                    res.json({info: 'cat updated successfully'});
+                });
+            }else{
+                res.json({info: 'cat not found'});
+            }
+        });
     });
 
     // Delete
     app.delete('/cat/:id', function(req, res){
-        _.remove(_cats, function(cat){
-            return cat.name === req.params.id;
+        Cat.findByIdAndRemove(req.params.id, function(err){
+            if (err) {
+                res.json({info: 'error during remove cat', error: err});
+            }
+            res.json({info: 'cat removed successfully'});
         });
-        res.json({info: 'cat removed successfully'});
     });
 
 };
